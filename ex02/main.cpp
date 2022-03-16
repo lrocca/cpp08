2e6846f3d128afe8d9755b88f1bf31aa52b3633e
Leonardo Rocca <lrocca@student.42roma.it>
2022-03-16 19:21:15 +0100

    ex02: test against vector

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,29 +1,62 @@
 #include <iostream>
+#include <vector>
 
 #include "MutantStack.hpp"
 
-int main()
+void	testVector()
 {
-	MutantStack<int> mstack;
+	std::vector<int>	mstack;
+
+	mstack.push_back(5);
+	mstack.push_back(17);
+
+	std::cout << "top\t" << *(mstack.end() - 1) << std::endl;
+
+	mstack.pop_back();
+
+	std::cout << "size\t" << mstack.size() << std::endl;
+
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	mstack.push_back(-42);
+	mstack.push_back(0);
+
+	std::vector<int>::iterator	it = mstack.begin();
+	std::vector<int>::iterator	ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+}
+
+void	testMutantStack()
+{
+	MutantStack<int>	mstack;
 
 	mstack.push(5);
 	mstack.push(17);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top\t" << mstack.top() << std::endl;
 
 	mstack.pop();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size\t" << mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
-
+	mstack.push(-42);
 	mstack.push(0);
 
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator	it = mstack.begin();
+	MutantStack<int>::iterator	ite = mstack.end();
+
 	++it;
 	--it;
 
@@ -33,7 +66,14 @@ int main()
 		++it;
 	}
 
-	std::stack<int> s(mstack);
+	std::stack <int>	s(mstack);
+}
 
-	return 0;
+int	main()
+{
+	std::cout << "*** vector ***" << std::endl;
+	testVector();
+
+	std::cout << std::endl << "*** mutantStack ***" << std::endl;
+	testMutantStack();
 }
